b4bdf1b0bb68be8bbcaa43747db2d7861e9b0716
Leo Jiang <leo.jiang.dev@gmail.com>
2015-03-24 11:43:03 +0800

    add a gist for a trick tip related to table view.

---
+++ iOS/fundamental/TableView/not_show_separator.m
@@ -0,0 +1,3 @@
+// set the table footer view with zero size view
+// to stop the continuous show of separators in rest of table.
+self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
